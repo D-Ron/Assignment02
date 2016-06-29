6f3ff3041d55e286a229b43642e813bdf64b0b9b
D Ron <Kara@Kara-MacBook-Pro.local>
2016-06-28 23:27:28 -0700

    revision scriptP1 and scriptP2_1

--- js/scriptAs2_P1.js
+++ js/scriptAs2_P1.js
@@ -5,7 +5,7 @@ var userName = window.prompt("What is your name?");
 */
 
 /*  ---------------------- 
-//Task 2  lecture 2 - slide 6
+//Task 2  
 var userName = window.prompt("What is your name?");
 var digit = window.prompt ("Pick a number");
     console.log(userName.charAt(digit));
@@ -13,7 +13,7 @@ var digit = window.prompt ("Pick a number");
 
 
 /*  ----------------------
-//Task 3  lecture 2 - slide 8
+//Task 3 
 var firstName = window.prompt("What is your first name?");
 var lastName = window.prompt("What is your last name?");
 var userName = firstName.concat(" " + lastName);
@@ -22,21 +22,21 @@ var userName = firstName.concat(" " + lastName);
 
 
 /* ----------------------
-//Task 4     lecture 2 - slide 10
+//Task 4  
 var sentence = "The quick brown fox jumped over the lazy dog.";
     window.alert(sentence.indexOf('fox'));
  */
 
 
 /* ----------------------
-//Task 5     lecture 2 - slide 11  
+//Task 5   
 var sentence = "The quick brown fox jumped over the lazy dog.";
     window.alert(sentence.lastIndexOf('fox'));
 */
 
 
 /*  ----------------------
-//Task 6     lecture 2 - slide 14   
+//Task 6   
 var sentence = "The quick brown fox jumped over the lazy dog.";
 var fullName = prompt('What is your full name?');
     window.alert(sentence.replace('the lazy dog', fullName));
@@ -44,7 +44,7 @@ var fullName = prompt('What is your full name?');
 
 
 /*  ----------------------
-//Task 7     lecture 2 - slide 15    
+//Task 7    
 var sentence = "The quick brown fox jumped over the lazy dog.";
 var pickWord = prompt('Please select 1 word from the following sentence: \n\"The quick brown fox jumped over the lazy dog.\" ');
     window.alert(sentence.search(pickWord));
@@ -53,7 +53,7 @@ var pickWord = prompt('Please select 1 word from the following sentence: \n\"The
 
 
 /*  ---------------------- 
-//Task 8     lecture 2 - slide 16-18     
+//Task 8    
 var old_string = "The quick brown fox jumped over the lazy dog.";
     //window.console.log(old_string.indexOf('the lazy dog')); // index of 32
     //window.console.log(old_string.indexOf('.')); // index of 44
@@ -63,19 +63,22 @@ var new_string = old_string.slice(32, 45);
 
 
 /*  ---------------------- 
-//Task 9       lecture 2 slide 20 and 25 
+//Task 9  
 var sentence = "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.          ";
 var sentence =  sentence.toLowerCase();
     window.alert(sentence.trim());
 */
 
 
-/*  ---------------------- 
-//Task 10      lecture 2  (lecture 3 slide 27?)
-var sentence = "the quick brown fox jumped over the lazy dog.";
-    window.alert(sentence.replace('t', 'T'));
-
+/*  ----------------------  
+//Task 10 
+var sentence; 
+    sentence = prompt("Please enter a sentence of your choosing.")
+    sentence = sentence.toUpperCase().slice(0, 1) + sentence.slice(1);
+    window.console.log(sentence);
 */
+
+
 
 
 // End Tasks for Assignment 2 part 1
--- js/scriptAs2_P2_1.js
+++ js/scriptAs2_P2_1.js
@@ -24,8 +24,30 @@ var pickNum = window.prompt('Please pick a number');
 */  
 
 
-/*  ----------------------
+/*  ---------------------- 
 // Task 4       lecture 3 slide 46, 47      
+// Version 2 of finding the max and min of a series of numbers
+
+
+    var pickNum = window.prompt('Please pick 5 numbers \nEx: 1, 2, 3, 4, 5');
+    var pickNumSplit = pickNum.split(",");
+    var digits = []; 
+            for (i = 0; i < pickNumSplit.length; i++) {
+                var numberConversion = parseInt(pickNumSplit[i]);
+                digits.push(numberConversion);    
+            }
+
+    var numberArrayAsNumbers = [4, 10, 4, 6, 7];
+
+    window.console.log(digits);
+    window.console.log(Math.max(digits[0], digits[1], digits[2], digits[3], digits[4]));
+    window.console.log(Math.min(digits[0], digits[1], digits[2], digits[3], digits[4]));
+ */ 
+
+
+
+// Task 4  
+/* Version 1 of finding the max and min of a series of numbers
 var pickNum = window.alert('Please pick 5 numbers \nEx: 1, 2, 3, 4, 5');
 var pickNum1 = window.prompt('1st pick');
 var pickNum2 = window.prompt('2nd pick');
@@ -37,15 +59,15 @@ var digits = [pickNum1, pickNum2, pickNum3, pickNum4, pickNum5];
     window.console.log(digits);
     window.console.log(Math.max(pickNum1, pickNum2, pickNum3, pickNum4, pickNum5));
     window.console.log(Math.min(pickNum1, pickNum2, pickNum3, pickNum4, pickNum5));  
-*/ 
+ */
 
 
 /* ----------------------
+
 // Task 5       lecture 2 slide 51       
 var pickNum = window.prompt('Please pick a number');
     window.console.log(Math.sqrt(pickNum));
 */
-
 
 // End Tasks for Assignment 2 part 2.1
 
